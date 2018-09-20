f475a79a7fad4c08f19b2fa5ebb902cfaf7503cd
Ben Sevcik <43259346+bsevcik@users.noreply.github.com>
2018-09-20 14:51:05 -0700

    Fixed check in bug and and added sort
    Fixed check in bug which pushed an updated return time every click; now it can only be returned once. Added clickable name button that sorts the array by first name. Capitalized first letter of every name before writing it to checkedInArray.

---
+++ scripts/script.js
@@ -0,0 +1,80 @@
+/*eslint-env browser*/
+
+
+// This array stores all the inputted data. Is gets output as a table body when running function displayArray()
+var checkedInArray = [];
+
+            
+//This function is meant to clear the table after the form is submitted. It wasn't reliable when using tableRow.length, so I set it to delete as many items as are in the array. The form data is pushed to the array before clearTable() is run
+function clearTable() {
+    "use strict";
+    for (var i = 0; i < checkedInArray.length; i++)
+        document.getElementById("checkedIn").deleteRow(-1);
+  }
+
+
+//This function writes the checkedInArray[] into an HTML table body, the table heading is hardcoded into the html file
+function displayArray() {
+    "use strict";
+    var i = 0
+    for (clearTable(); i < checkedInArray.length; i++) {
+        document.getElementById("checkedIn").innerHTML += "<tr><td>" + checkedInArray[i][0] + "</td> <td>" + checkedInArray[i][1] + "</td> <td>" + checkedInArray[i][2] + "</td><td>" + checkedInArray[i][3] + "</td><td>" + checkedInArray[i][4] + "</td></tr>";
+    }
+}
+
+/*This part of the code simply wrote the user typed data into a table, but did not write it to an array. It is now deprecated.
+            document.getElementById("checkedIn").innerHTML += "<tr><td>" + name + "</td> <td>" + badge + "</td> <td>" + badgeNum + "</td><td>" + timeNow + '</td><td><input type="button" class="return" value="Click to Return Badge"></td>';
+*/
+
+//This function validates that something was typed into all three input boxes, capitolizes the first letter of writes all the data typed in by the user to checkedInArray, runs clearTable() to clear the table, and then runs displayArray() to write a the updated array into the table body
+document.getElementById("checkIn").addEventListener("click",
+    function () {
+        "use strict";
+        var time = new Date();
+        var timeNow = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+        if (document.getElementById("nameTyped").value === "" || document.getElementById("badgeTyped").value === "" || document.getElementById("badgeNumTyped").value === "") {
+            window.alert("You need to fill out all three forms");
+        } else {
+            var name = document.getElementById("nameTyped").value;
+            var nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+            checkedInArray.push([nameCapitalized, document.getElementById("badgeTyped").value, document.getElementById("badgeNumTyped").value, timeNow, "<input onclick='returnBadge(this)' type='button' class='tableButton' value='Click to Return Badge'>"]);
+            clearTable();
+            displayArray();
+//            window.console.log(checkedInArray);
+
+
+//This clears the written input forms after the information is written to checkedInArray
+            document.getElementById("nameTyped").value = "";
+            document.getElementById("badgeTyped").value = "";
+            document.getElementById("badgeNumTyped").value = "";
+        }
+        
+    }
+);
+
+function returnBadge(r) {
+    var i = r.parentElement.parentElement.rowIndex - 1;
+//    window.console.log(i);
+    var time = new Date();
+    var timeNow = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+//    window.console.log(r.parentElement);
+    r.parentElement.innerHTML = timeNow;
+//    window.console.log(checkedInArray[i]);
+    checkedInArray[i][4] = timeNow;
+//    This would let you delete the row of the HTML table clicked on
+//    document.getElementById("checkedIn").deleteRow(i);
+
+//    document.getElementById("checkedIn").rows[i].childNodes[6].innerHTML = timeNow;
+// This lets you change the button of the first html table row into "testing"
+// document.getElementById("checkedIn").childNodes[5].childNodes[0].childNodes[6].innerHTML = "testing"
+    
+}
+
+// This sorts checkedInArray alphabetically by name
+document.getElementById("nameButton").addEventListener("click",
+    function nameSort() {
+        checkedInArray.sort();
+        clearTable();
+        displayArray();
+    }
+                                                     )
